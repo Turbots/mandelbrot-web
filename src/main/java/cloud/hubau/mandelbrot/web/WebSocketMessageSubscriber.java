ed7bbb37a60ce1ba02624ec4518a65b40980a886
Dieter Hubau <dhubau@gmail.com>
2018-10-04 02:25:31 +0200

    prettier design - v4

--- src/main/java/cloud/hubau/mandelbrot/web/WebSocketMessageSubscriber.java
+++ src/main/java/cloud/hubau/mandelbrot/web/WebSocketMessageSubscriber.java
@@ -1,12 +1,13 @@
 package cloud.hubau.mandelbrot.web;
 
+import io.netty.channel.ChannelOption;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.http.client.reactive.ReactorClientHttpConnector;
 import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Flux;
-import reactor.core.publisher.Mono;
 import reactor.core.publisher.UnicastProcessor;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
@@ -24,36 +25,39 @@ import java.time.temporal.ChronoUnit;
 	WebSocketMessageSubscriber(UnicastProcessor<Event> eventPublisher) {
 		this.eventPublisher = eventPublisher;
 		webClient = WebClient.builder().baseUrl("http://35.241.137.76")
+			.clientConnector(
+				new ReactorClientHttpConnector(o -> o.option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 600000)))
 			.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
 			.defaultHeader(HttpHeaders.HOST, "mandelbrot.default.example.com")
 			.build();
 	}
 
-	void onNext(Event event) {
-		if (event.getType() == Event.Type.CALCULATION) {
+	void onNext(Event e) {
+		if (e.getType() == Event.Type.CALCULATION) {
+			log.info("Generating Grid [({},{}),({},{})] with size [{},{}]", e.getX1(), e.getY1(), e.getX2(), e.getY2(),
+				SIZE, SIZE);
+
 			Flux<Integer> xFlux = Flux.range(0, SIZE);
 			Flux<Integer> yFlux = Flux.range(0, SIZE);
 
 			Flux<Tuple2<Integer, Integer>> tupleFlux = xFlux.flatMap(x -> yFlux.map(y -> Tuples.of(x, y)));
 
 			tupleFlux
-				.zipWith(Flux.interval(Duration.of(10, ChronoUnit.MILLIS)))
+				.zipWith(Flux.interval(Duration.of(50, ChronoUnit.MILLIS)))
 				.map(t ->
-					calculate(event.getX1(), event.getY1(), event.getX2(), event.getY2(), t.getT1().getT1(),
-						t.getT1().getT2(), event.getDepth())
-						.subscribe(pixel -> {
-							//log.info("(" + t.getT1().getT1() + "," + t.getT1().getT2() + ") : " + pixel.getColor());
-							eventPublisher.onNext(
-								new Event(Event.Type.RESULT, t.getT1().getT1(), t.getT1().getT2(), 0, 0, 0,
-									pixel.getColor()));
-						}))
+					calculate(e.getX1(), e.getY1(), e.getX2(), e.getY2(), t.getT1().getT1(),
+						t.getT1().getT2(), e.getDepth())
+						.subscribe(pixel -> eventPublisher.onNext(
+							new Event(Event.Type.RESULT, t.getT1().getT1(), t.getT1().getT2(), 0, 0, 0,
+								pixel.getColor()))))
+				.buffer(100)
 				.doOnComplete(() -> log.info("Done."))
 				.subscribe();
 		}
-		eventPublisher.onNext(event);
+		eventPublisher.onNext(e);
 	}
 
-	private Mono<ColoredPixel> calculate(double x1, double y1, double x2, double y2, double x, double y, int depth) {
+	private Flux<ColoredPixel> calculate(double x1, double y1, double x2, double y2, double x, double y, int depth) {
 		double stepX = (x2 - x1) / SIZE;
 		double stepY = (y2 - y1) / SIZE;
 		double translatedX = x * stepX;
@@ -62,13 +66,12 @@ import java.time.temporal.ChronoUnit;
 		double imaginary = y1 + translatedY;
 
 		Calculation calculation = new Calculation(real, imaginary, depth * 255);
-		//log.info("Calculating (" + real + "," + imaginary + ")");
 
 		return this.webClient
 			.post()
 			.body(BodyInserters.fromObject(calculation))
 			.retrieve()
-			.bodyToMono(ColoredPixel.class);
+			.bodyToFlux(ColoredPixel.class);
 	}
 
 	void onError(Throwable error) {
